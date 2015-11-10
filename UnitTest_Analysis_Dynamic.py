2aa802b8f265971da6f401aeb5ec3e7b96a3c00a
Ron Rahaman <rahaman@cs.uchicago.edu>
2015-11-10 13:18:05 -0600

    Refactored RunTestFactory to create new classes from a base class

--- UnitTest_Analysis_Dynamic.py
+++ UnitTest_Analysis_Dynamic.py
@@ -36,6 +36,36 @@ import unittest
 #                             cls.foundVals[valName] = cls.missingVals.pop(valName)
 #                             cls.foundVals[valName]['test'] = testVal
 
+class RunTestBase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open(cls.logfile, 'r') as fd:
+            for line in fd:
+                # Can't use dictionary iterator, since missingTests can change during loop.
+                # Need to iterate over list returned by missngTests.keys()
+                for testName in cls.missingTests.keys():
+                    if testName in line:
+                        try:
+                            col = -cls.missingTests[testName]['col']
+                            testVal = float(line.split()[col])
+                        except (ValueError,IndexError) :
+                            pass
+                        else:
+                            cls.foundTests[testName] = cls.missingTests.pop(testName)
+                            cls.foundTests[testName]['testVal'] = testVal
+
+    @classmethod
+    def tearDownClass(cls):
+        if len(cls.missingTests) > 0:
+            print("[%s]...I couldn't find all the requested value in the log file..."%cls.exampleName)
+            testList = ", ".join(cls.missingTests)
+            print("[%s]...%s were not found..."%(cls.exampleName,testList))
+            print("%s : F "%cls.exampleName)
+        else :
+            print("%s : ."%cls.exampleName)
+
+
 def RunTestFactory(exampleName, logfile, listOfVals):
 
     # Convenient data structure
@@ -43,53 +73,29 @@ def RunTestFactory(exampleName, logfile, listOfVals):
         [(testName, {'target': target, 'tolerance': tolerance, 'col': col})
          for (testName, target, tolerance, col) in listOfVals ] )
 
-    # Get a new subclass of TestCase
+    # Get a new subclass of RunTestBase
     validName = re.sub(r'[_\W]+', '_', 'NekTest_%s' % exampleName)
-    cls = type(validName, (unittest.TestCase,), {})
+    cls = type(validName, (RunTestBase,), {})
+
+    # Set class attributes
+    cls.logfile = logfile
+    cls.exampleName = exampleName
+    cls.missingTests = dictOfVals
+    cls.foundTests = {}
 
     # Add a test function for each test
     for (i, testName) in enumerate(dictOfVals):
 
         def testFunc(self, testName=testName):
+            cls = self.__class__
             self.assertIn(testName, cls.foundTests)
             print("[%s] %s : %s" %
-                  (exampleName, testName, cls.foundTests[testName]['testVal']))
+                  (cls.exampleName, testName, cls.foundTests[testName]['testVal']))
             self.assertLess(abs(cls.foundTests[testName]['testVal'] - cls.foundTests[testName]['target']),
                             cls.foundTests[testName]['tolerance'])
 
         validName = re.sub(r'[_\W]+', '_', 'test_%s_%02d' % (testName, i))
         setattr(cls, validName, testFunc)
-
-    # Find each test in the logfile
-    cls.missingTests = dictOfVals
-    cls.foundTests = {}
-
-    with open(logfile, 'r') as fd:
-        for line in fd:
-            # Can't use dictionary iterator, since missingTests can change during loop.
-            # Need to iterate over list returned by missngTests.keys()
-            for testName in cls.missingTests.keys():
-                if testName in line:
-                    try:
-                        col = -cls.missingTests[testName]['col']
-                        testVal = float(line.split()[col])
-                    except (ValueError,IndexError) :
-                        pass
-                    else:
-                        cls.foundTests[testName] = cls.missingTests.pop(testName)
-                        cls.foundTests[testName]['testVal'] = testVal
-
-    # Teardown phase
-    def tearDownClass(cls):
-        if len(cls.missingTests) > 0:
-            print("[%s]...I couldn't find all the requested value in the log file..."%exampleName)
-            testList = ", ".join(cls.missingTests)
-            print("[%s]...%s were not found..."%(exampleName,testList))
-            print("%s : F "%exampleName)
-        else :
-            print("%s : ."%exampleName)
-
-    cls.tearDownClass = classmethod(tearDownClass)
 
     return cls
 
